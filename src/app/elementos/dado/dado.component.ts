19e2634e90f441a74a54da1600d51bba7b562fd8
jimmy cruz <cruzperezjimmya@gmail.com>
2022-12-29 10:53:05 -0500

    maquetacion del dado

---
+++ src/app/elementos/dado/dado.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dado',
+  templateUrl: './dado.component.html',
+  styleUrls: ['./dado.component.css']
+})
+export class DadoComponent {
+  resultado:number = 0;
+
+
+
+}
