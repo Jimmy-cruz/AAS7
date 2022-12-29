ec5ae21ee168ee2800e6cf2ba51d9a161948fde9
jimmy cruz <cruzperezjimmya@gmail.com>
2022-12-29 12:29:52 -0500

    se agrega un mensaje que almacena el ultimo resultado obtenido

--- src/app/elementos/dado/dado.component.ts
+++ src/app/elementos/dado/dado.component.ts
@@ -9,24 +9,34 @@ export class DadoComponent {
   // declaramos las variables de resultado y condicional para ejecutar la funcion
   resultado:number = 0;
   condicional:number = 1;
+  
+  // declaramos una variable que almacene el ultimo resultado obtenido
+  resultadoAnterior:number = this.resultado;
 
   // declaramos la funcion lanzar()
   lanzar(){
     // creamos un numero random entero positivo y le asignamos a una variable
     this.condicional = Math.round(Math.random()*10) ;
 
+    // asignamos el ultimo resultado obtenido a la variable para imprimirlo en la vista
+    this.resultadoAnterior = this.resultado;
+
     // creamos una condicional que valide si el resultado y el valor aleatorio son iguales
     // de desta forma solo se establecera el resultadi unicamente cuando sean diferentes y no repitira el mismo valor
     if (this.condicional != this.resultado) {
+
       // creamos otra condicional la cual nos establecera un rango entre el 1 y el 6
       if (this.condicional!= 0 && this.condicional <7) {
+
         // asignamos el valor de la condicional al resultado para imprimirlo en la vista
         this.resultado = this.condicional;
       }else{
+
         // en caso no sea valida la condicion se ejecutara la funcion nuevamente
         this.lanzar();
       }
     }else{
+
       // en caso no sea valida la condicion se ejecutara la funcion nuevamente
       this.lanzar();
     }
